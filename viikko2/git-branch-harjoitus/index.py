149ccc66ba7c0da1fa7b2dedda7a01112d9c4346
JoonaPietarinen <joonapietarinen3@gmail.com>
2025-11-10 16:30:49 +0200

    muutos

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -10,6 +10,6 @@ x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 print(f"{x} + {y} = {summa(x, y)}") # muutos mainissa
 print(f"{x} - {y} = {erotus(x, y)}") # muutos mainissa
-
+#
 logger("lopetetaan")
 print("goodbye!") # lisäys bugikorjaus-branchissa
